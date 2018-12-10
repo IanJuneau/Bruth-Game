dc6b4c715f93c6cfa3a9d29dee7a9e4341d1e9b0
Payton J <kyplerr@gmail.com>
2018-12-09 16:20:35 -0800

    Added More Libs

--- src/bruth/game/gfx/SpriteSheet.java
+++ src/bruth/game/gfx/SpriteSheet.java
@@ -1,19 +1,13 @@
 package bruth.game.gfx;
 
-import java.awt.image.BufferedImage;
+import org.joml.Matrix4f;
 
 public class SpriteSheet {
-	public int width;
-	public int height;
-	public int[] pixels;
+	private Texture texture;
+	private Matrix4f scale;
+	private Matrix4f translation;
 	
-	public SpriteSheet(BufferedImage image) {
-		width = image.getWidth();
-		height = image.getHeight();
-		pixels = image.getRGB(0, 0, width, height, null, 0, width);
-		
-		for(int i = 0; i < pixels.length; i++) {
-			pixels[i] = (pixels[i] & 0xff) / 64;
-		}
+	public SpriteSheet() {
+
 	}
 }
